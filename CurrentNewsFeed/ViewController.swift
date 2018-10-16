b5e662a3cc41cc178b5563449c0f746a5d4d4314
Matheus Oliveira Costa <matheusoliveiracos@gmail.com>
2018-10-16 09:54:36 -0300

    First commit

---
+++ CurrentNewsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CurrentNewsFeed
+//
+//  Created by Matheus Costa on 16/10/18.
+//  Copyright © 2018 Matheus Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
